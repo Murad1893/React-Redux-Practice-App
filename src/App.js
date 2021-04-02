a4cf2d1c1906025308f3dfaa334994a9d775c2c9
Murad1893 <muradmansoor189@gmail.com>
2021-04-03 02:43:27 +0500

    Added counter onClick functions, counterActions

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { useSelector, useDispatch } from 'react-redux'
+import { increment, decrement } from './actions/counterActions'
 
 function App() {
+  // we can access the state variables using the useSelector
+  const counter = useSelector((state) => state.counter)
+
+  // we can dispatch action using this hook
+  const dispatch = useDispatch()
+
   return (
     <div className="App">
-      <h1>Murad</h1>
+      <h1>Counter {counter}</h1>
+      <div className='container'>
+        <button onClick={() => dispatch(increment())}>+</button>
+        <button onClick={() => dispatch(decrement())}>-</button>
+      </div>
     </div>
   );
 }
---
+++ src/actions/counterActions.js
@@ -0,0 +1,13 @@
+// we define action here that will trigger the case statements in the reducers
+
+export const increment = () => {
+  return {
+    type: 'INCREMENT'
+  }
+}
+
+export const decrement = () => {
+  return {
+    type: 'DECREMENT'
+  }
+}
